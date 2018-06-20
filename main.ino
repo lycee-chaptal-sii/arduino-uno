dee36e2bdff4962e849af66494b62bb2e17c46c5
atrantan <atrantan@gmail.com>
2018-06-20 19:19:47 +0200

    ajout fichiers moteurs

--- main.ino
+++ main.ino
@@ -2,13 +2,33 @@
 //
 
 #include "Arduino.h"
+#include "include/Grove_I2C_Motor_Driver.h"
 
-// the setup function runs once when you press reset or power the board
+// default I2C address is 0x0A
+#define I2C_ADDRESS 0x0A
+
 void setup()
 {
+    Motor.begin(I2C_ADDRESS);
 }
 
-// the loop function runs over and over again forever
 void loop()
 {
+    // Set speed of MOTOR1, Clockwise
+    Motor.speed(MOTOR1, 50);
+    // Set speed of MOTOR2, Anticlockwise
+    Motor.speed(MOTOR2, -70);
+    delay(2000);
+
+    // Change speed and direction of MOTOR1
+    Motor.speed(MOTOR1, -100);
+    // Change speed and direction of MOTOR2
+    Motor.speed(MOTOR2, 100);
+    delay(2000);
+
+    // Stop MOTOR1 and MOTOR2
+    Motor.stop(MOTOR1);
+    Motor.stop(MOTOR2);
+    delay(2000);
+
 }
